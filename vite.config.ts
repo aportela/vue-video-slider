d36b8187b3f184033295631cad86b3890923078c
alex <766f6964+github@gmail.com>
2024-02-22 03:00:04 +0100

    changed dist build output

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,14 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
+    /*
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
       }
     }
+    */
   }
 })
